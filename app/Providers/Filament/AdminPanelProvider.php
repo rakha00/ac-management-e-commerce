2937361930c06742ecaca1a84a9940cd9b8a16f2
Rakha <rkhdinar00@gmail.com>
2025-11-05 12:29:27 +0700

    feat(admin): customize filament admin panel appearance
    Add font, theme mode, logo and favicon configuration to improve visual identity and user experience

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers\Filament;
 
 use App\Http\Middleware\CheckKaryawanStatus;
+use Filament\Enums\ThemeMode;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -73,6 +74,13 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
                 CheckKaryawanStatus::class,
-            ]);
+            ])
+            ->font('Poppins')
+            ->defaultThemeMode(ThemeMode::Light)
+            ->darkMode(false)
+            ->brandLogo(asset('img/GSI-landscape.png'))
+            ->brandLogoHeight('3rem')
+            ->favicon(asset('img/GSI.png'));
+
     }
 }
